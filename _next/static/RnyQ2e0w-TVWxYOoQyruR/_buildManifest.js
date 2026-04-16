e92714bff76309465ba515b507e9e042ab082973
abotescu20-maker <a.botescu20@s.bio.unibuc.ro>
2026-04-16 11:49:39 +0300

    Updates

---
+++ _next/static/RnyQ2e0w-TVWxYOoQyruR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/adflow//_next/:path+",
+        "destination": "/adflow/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
